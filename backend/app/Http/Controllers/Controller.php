233c6a7f641a0feda209558c092420932d5893e2
obel <debebe.robel@gmail.com>
2025-11-11 17:47:36 +0300

    Fix Swagger HTTPS: Force HTTPS URLs and update server URL

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use OpenApi\Attributes as OA;
 
 #[OA\Info(title: 'HyperSwipe API', version: '1.0.0')]
-#[OA\Server(url: 'http://localhost/api', description: 'Local')]
+#[OA\Server(url: 'https://tinderphp-production.up.railway.app/api', description: 'Production')]
 abstract class Controller
 {
 }
